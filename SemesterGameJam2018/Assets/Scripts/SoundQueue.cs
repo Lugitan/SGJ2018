ba75707ab3dcf401a7e5dc93cf59fbe7398bca70
Gilles Tanson <gillestanson@googlemail.com>
2018-12-01 01:20:21 +0100

    Added FirstPersonController and started working on SoundQueue Class

---
+++ SemesterGameJam2018/Assets/Scripts/SoundQueue.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundQueue : MonoBehaviour {
+
+    private Vector3 position = new Vector3(0, 0, 0);
+    private float intensity = 1.0f;
+    private AudioSource sound = null;
+
+    public AudioSource Sound
+    {
+        get
+        {
+            return sound;
+        }
+
+        set
+        {
+            sound = value;
+        }
+    }
+
+    public float Intensity
+    {
+        get
+        {
+            return intensity;
+        }
+
+        set
+        {
+            intensity = value;
+        }
+    }
+
+    public Vector3 Position
+    {
+        get
+        {
+            return Position1;
+        }
+
+        set
+        {
+            Position1 = value;
+        }
+    }
+
+    public Vector3 Position1
+    {
+        get
+        {
+            return position;
+        }
+
+        set
+        {
+            position = value;
+        }
+    }
+
+}
